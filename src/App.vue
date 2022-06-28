b6ee97f8308607f4cb602024894cb22bff292988
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-29 01:07:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <NavBar />
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
   <router-view />
 </template>
 
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,20 +1,52 @@
 <template>
   <!-- Dropdown Structure -->
-  <ul id="dropdown1" class="dropdown-content">
-    <li><a href="#!">one</a></li>
-    <li><a href="#!">two</a></li>
+  <ul id="dropdown" class="dropdown-content">
+    <li><a href="#!" style="color: #039be5">one</a></li>
+    <li><a href="#!" style="color: #039be5">two</a></li>
     <li class="divider"></li>
-    <li><a href="#!">three</a></li>
+    <li><a href="#!" style="color: #039be5">three</a></li>
+  </ul>
+  <!-- sidenav structure -->
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="mobile.html">Mobile</a></li>
+    <li>
+      <select>
+        <option value="" disabled selected>Choose your option</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </li>
   </ul>
   <nav>
-    <div class="nav-wrapper" style="background: linear-gradient(139deg, rgba(1,87,155,1) 0%, rgba(3,155,229,1) 87%);">
-      <router-link to="/" style="margin-left: 1%; margin-top: -4px" class="brand-logo left">Logo</router-link>
+    <div
+      class="nav-wrapper"
+      style="
+        background: linear-gradient(
+          139deg,
+          rgba(1, 87, 155, 1) 0%,
+          rgba(3, 155, 229, 1) 87%
+        );
+      "
+    >
+      <router-link
+        to="/"
+        style="margin-left: 1%; margin-top: -4px"
+        class="brand-logo left"
+        >Logo</router-link
+      >
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger right"
+        ><i class="material-icons">menu</i></a
+      >
       <ul class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
         <!-- Dropdown Trigger -->
         <li>
-          <a class="dropdown-trigger" href="#!" data-target="dropdown1"
+          <a class="dropdown-trigger" href="#!" data-target="dropdown"
             >Dropdown<i class="material-icons right">arrow_drop_down</i></a
           >
         </li>
