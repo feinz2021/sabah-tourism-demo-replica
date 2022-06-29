f5fe532983aa17c6bc3d9e99f9a13d04e5d66741
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-29 14:26:05 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,32 @@
 <template>
-  <div
-    style="
-      background-image: url('../assets/fahrul-azmi-JK8f7i1L4bc-unsplash.jpg');
-    "
-  ></div>
   <div class="container">
+    <div class="carousel">
+      <a class="carousel-item" href="#one!"
+        ><img src="../assets/andy-wang-wvd6teqldgA-unsplash.jpg"
+      /></a>
+      <a class="carousel-item" href="#two!"
+        ><img src="../assets/fahrul-azmi-JK8f7i1L4bc-unsplash.jpg"
+      /></a>
+      <a class="carousel-item" href="#three!"
+        ><img src="../assets/haydn-golden-GRRdN9pM2d4-unsplash.jpg"
+      /></a>
+      <a class="carousel-item" href="#four!"
+        ><img src="../assets/joel-brunner-mNWDTud3AaI-unsplash.jpg"
+      /></a>
+      <a class="carousel-item" href="#five!"
+        ><img src="../assets/ling-tang-XjUhALD9HO4-unsplash.jpg"
+      /></a>
+    </div>
+
     <div class="row">
       <!-- image 1 -->
-
       <div class="col s12 m6 l4">
-        <div class="card" data-aos="fade-right">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="300"
+          data-aos-easing="ease-in-sine"
+        >
           <div class="card-image">
             <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
             <span class="card-title"
@@ -39,7 +56,152 @@
       </div>
       <!-- image 2 -->
       <div class="col s12 m6 l4">
-        <div class="card">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="600"
+          data-aos-easing="ease-in-sine"
+        >
+          <div class="card-image">
+            <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
+            <span class="card-title"
+              >Photo by
+              <a
+                href="https://unsplash.com/@ryanoniel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Ryan 'O' Niel</a
+              >
+              on
+              <a
+                href="https://unsplash.com/s/photos/sabah?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Unsplash</a
+              ></span
+            >
+          </div>
+          <div class="card-content">
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+      <!-- image 3 -->
+      <div class="col s12 m6 l4">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="900"
+          data-aos-easing="ease-in-sine"
+        >
+          <div class="card-image">
+            <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
+            <span class="card-title"
+              >Photo by
+              <a
+                href="https://unsplash.com/@ryanoniel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Ryan 'O' Niel</a
+              >
+              on
+              <a
+                href="https://unsplash.com/s/photos/sabah?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Unsplash</a
+              ></span
+            >
+          </div>
+          <div class="card-content">
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+      <!-- image 4 -->
+      <div class="col s12 m6 l4">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="1200"
+          data-aos-easing="ease-in-sine"
+        >
+          <div class="card-image">
+            <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
+            <span class="card-title"
+              >Photo by
+              <a
+                href="https://unsplash.com/@ryanoniel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Ryan 'O' Niel</a
+              >
+              on
+              <a
+                href="https://unsplash.com/s/photos/sabah?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Unsplash</a
+              ></span
+            >
+          </div>
+          <div class="card-content">
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+      <!-- image 5 -->
+      <div class="col s12 m6 l4">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="1500"
+          data-aos-easing="ease-in-sine"
+        >
+          <div class="card-image">
+            <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
+            <span class="card-title"
+              >Photo by
+              <a
+                href="https://unsplash.com/@ryanoniel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Ryan 'O' Niel</a
+              >
+              on
+              <a
+                href="https://unsplash.com/s/photos/sabah?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >Unsplash</a
+              ></span
+            >
+          </div>
+          <div class="card-content">
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+      <!-- image 6 -->
+      <div class="col s12 m6 l4">
+        <div
+          class="card"
+          data-aos="fade-right"
+          data-aos-duration="1800"
+          data-aos-easing="ease-in-sine"
+        >
           <div class="card-image">
             <img src="../assets/ryan-o-niel-ywQLwGDrrZY-unsplash.jpg" />
             <span class="card-title"
@@ -73,10 +235,12 @@
 
 <script>
 import AOS from "../../node_modules/aos";
+// import M from "@materializecss/materialize";
 export default {
   components: {},
   mounted() {
     AOS.init();
+    window.M.AutoInit();
   },
 };
 </script>
@@ -91,5 +255,8 @@ body {
   background-attachment: fixed;
   background-size: cover;
   background-position: center;
+}
+.carousel .carousel-item {
+  width: 500px !important;
 }
 </style>
