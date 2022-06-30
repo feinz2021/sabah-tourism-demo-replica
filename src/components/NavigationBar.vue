eb474fb6670dbc787f998038fc0913cb18eb2acc
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-30 09:31:16 +0800

    update

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,44 +1,41 @@
 <template>
   <!-- Dropdown Structure -->
   <ul id="dropdown" class="dropdown-content">
-    <li><a href="#!" style="color: #00acc1">one</a></li>
-    <li><a href="#!" style="color: #00acc1">two</a></li>
-    <li class="divider"></li>
-    <li><a href="#!" style="color: #00acc1">three</a></li>
+    <li><a href="#!" style="color: #00acc1">Rural Tourism</a></li>
+    <li><a href="#!" style="color: #00acc1">Business Events</a></li>
   </ul>
   <!-- sidenav structure -->
   <ul class="sidenav" id="mobile">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
+    <li><a href="sass.html">Signature Attraction</a></li>
+    <li><a href="badges.html">Things To Do</a></li>
     <li>
+      <div style="margin-left: 10%">
       <select>
-        <option value="" disabled selected>Choose your option</option>
-        <option value="1">Option 1</option>
-        <option value="2">Option 2</option>
-        <option value="3">Option 3</option>
+        <option  value="" disabled selected>Niche Market</option>
+        <option value="1">Rural Tourism</option>
+        <option value="2">Business Events</option>
       </select>
+      </div>
     </li>
   </ul>
   <nav>
     <div class="nav-wrapper">
       <router-link
         to="/"
-        style="margin-left: 1%; margin-top: -2px"
-        class="brand-logo left"
+        style="margin-right: 1%; margin-top: -2px"
+        class="brand-logo right"
         >Sabah Tourism</router-link
       >
-      <a href="#" data-target="mobile" class="sidenav-trigger right"
+      <a href="#" data-target="mobile" class="sidenav-trigger left"
         ><i class="material-icons">menu</i></a
       >
-      <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="sass.html">Signature Attraction</a></li>
+        <li><a href="badges.html">Things To Do</a></li>
         <!-- Dropdown Trigger -->
         <li>
           <a class="dropdown-trigger" href="#!" data-target="dropdown"
-            >Dropdown<i class="material-icons right">arrow_drop_down</i></a
+            >Niche Market<i class="material-icons right">arrow_drop_down</i></a
           >
         </li>
       </ul>
