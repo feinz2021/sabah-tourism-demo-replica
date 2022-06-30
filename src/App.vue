af58291eb227952c521495b78d94d9a66f793b08
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-30 16:19:48 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <NavBar />
   <router-view />
+  <FooterPage />
 </template>
 
 <script>
 import NavBar from "@/components/NavigationBar.vue";
+import FooterPage from "@/components/FooterPage.vue";
 export default {
-  components: { NavBar },
+  components: { NavBar, FooterPage },
   data() {
     return {};
   },
